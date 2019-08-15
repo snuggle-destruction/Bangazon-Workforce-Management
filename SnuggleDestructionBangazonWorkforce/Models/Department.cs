1a2d6f5ac9b2fda918deaf074aceb933447f2314
Burak Koc <burakkayakoc@gmail.com>
2019-08-15 14:25:43 -0500

    Department Class created

---
+++ SnuggleDestructionBangazonWorkforce/Models/Department.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SnuggleDestructionBangazonWorkforce.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Budget { get; set; }
+        public int AssignedEmployees { get; set; }
+    }
+}
