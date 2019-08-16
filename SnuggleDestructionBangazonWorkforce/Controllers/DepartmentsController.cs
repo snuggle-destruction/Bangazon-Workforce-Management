43b3f43e844aea3ba1795c8d44e9cdd6e84347c1
Burak Koc <burakkayakoc@gmail.com>
2019-08-16 14:51:54 -0500

    Master merging

--- SnuggleDestructionBangazonWorkforce/Controllers/DepartmentsController.cs
+++ SnuggleDestructionBangazonWorkforce/Controllers/DepartmentsController.cs
@@ -146,52 +146,6 @@ namespace SnuggleDestructionBangazonWorkforce.Controllers
             }
         }
 
-        // GET: Departments/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: Departments/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Departments/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Departments/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
         private Department GetOneDepartment(int id)
         {
             using (SqlConnection conn = Connection)
