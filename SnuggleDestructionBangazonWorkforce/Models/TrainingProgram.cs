1793f7ace999456f3e071bece5f42c0b36801cbb
deliotclarke <deliotclarke@gmail.com>
2019-08-19 11:29:45 -0500

    added data annotations to Training Program Model.

--- SnuggleDestructionBangazonWorkforce/Models/TrainingProgram.cs
+++ SnuggleDestructionBangazonWorkforce/Models/TrainingProgram.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +8,20 @@ namespace SnuggleDestructionBangazonWorkforce.Models
 {
     public class TrainingProgram
     {
+        [Required]
+        [Display(Name = "Training Program Id")]
         public int Id { get; set; }
+        [Required]
+        [Display(Name = "Name")]
         public string Name { get; set; }
+        [Required]
+        [Display(Name = "Start Date")]
         public DateTime StartDate { get; set; }
+        [Required]
+        [Display(Name = "End Date")]
         public DateTime EndDate { get; set; }
+        [Required]
+        [Display(Name = "Maximum Attendees")]
         public int MaxAttendees { get; set; }
         public List<Employee> AttendingEmployees { get; set; }
 
